9724ca5b09a88bb39b4ab846c6d5dea44e094c07
Sébastien Verslype <sebastien.verslype@gmail.com>
2023-01-17 11:39:44 +0100

    Co-authored-by: Angelik92 <Angelik92@users.noreply.github.com> Co-authored-by: marion9082 <marion9082@users.noreply.github.com> Co-authored-by: huipfe <huipfe@users.noreply.github.com> Co-authored-by: MaryelN <MaryelN@users.noreply.github.com> Co-authored-by: MaryJyl <MaryJyl@users.noreply.github.com> Co-authored-by: Vehqa <Vehqa@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//on va coder avec le cul, ok
+
+// Definir la variable de nos div "question-block"
+const questionContainer = document.querySelectorAll(".question-block");
+
+// Definir la variable de nos inputs
+
+const inputRadio = document.querySelectorAll('input');
+// Definir un tableau contenant nos bonne reponses
+
+// const isTrue = ["a", "b", "a", "c"];
+
+// Chercher la value de notre const inputRadio
+
+// function inputArray() {
+
+//     //si notre "ID.value" est checked alors....
+//     for (let i = 0; i < inputRadio.length; i++){
+//         if (inputRadio[i].checked) {
+
+
+//         }
+//     }}
+
+//     inputArray();
+
 
